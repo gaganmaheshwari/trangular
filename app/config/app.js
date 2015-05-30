64a6bb4f93a4262523acdb87977e3a98cb080870
Gagan Maheshwari <gmaheshwari@paypal.com>
2015-05-30 10:12:19 -0700

    New directory structure and ui-router addition

--- app/config/app.js
+++ app/config/app.js
@@ -20,11 +20,11 @@ angular
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
+        templateUrl: '../views/main.html',
         controller: 'MainCtrl'
       })
       .when('/about', {
-        templateUrl: 'views/about.html',
+        templateUrl: '../views/about.html',
         controller: 'AboutCtrl'
       })
       .otherwise({
